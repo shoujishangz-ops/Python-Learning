82259e3c8bc3ba2fed4141d971039901de8d4331
shoujishangz-ops <shoujishangz@gmail.com>
2026-07-16 18:27:20 +0800

    complete day01 python basics

--- Python-Learning/hello.py
+++ Python-Learning/hello.py
@@ -1,6 +1,7 @@
-name = input("请输入你的名字:")
-ai_direction = input("请输入你喜欢的AI方向:")
+name = input("请输入用户名:")
+age = int(input("请输入年龄:"))
 
-print(f"你好，{name}！")
-print(f"你未来想学习{ai_direction}方向。")
-print(f"欢迎加入AI学习之路")
+if age >= 18:
+    print(f"你好,{name},你可以使用AI助手")
+else:
+    print(f"你好,{name},你暂时不能使用AI助手")
